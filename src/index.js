7d9b4d4a0dc208f6441dabb056ba79140019c3a5
zz <4449378965@qq.com>
2019-12-02 16:36:13 +0800

    update show events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+const chalk = require('chalk');
 class logPlugin {
   constructor(options) {
-    options = Object.assign({}, { showWords: false }, options);
+    options = Object.assign({}, { showWords: false, showEvent: true }, options);
     if (options.path === undefined && options.showWords) {
       options.path = './word.json';
     }
@@ -8,7 +9,7 @@ class logPlugin {
   }
   apply(compiler) {
     const pluginName = 'log';
-    const { path } = this.options;
+    const { path, showEvent } = this.options;
     let success = 0;
     let words = [];
     let events = require('./event.json');
@@ -28,7 +29,7 @@ class logPlugin {
       console.log('  少女乞讨中...');
     });
     compiler.hooks.failed.tap(pluginName, err => {
-      console.log('  少女卒，享年18。');
+      console.log(chalk.red('  少女卒，享年18。'));
     });
     compiler.hooks.done.tap(pluginName, compilation => {
       console.clear();
@@ -38,11 +39,13 @@ class logPlugin {
         const item = words[random];
         word = `\n\n     词：${item.word}  ${item.mean}  \n`;
       }
-      const random = ~~(Math.random() * events.length);
-      const item = events[random];
-      event = item.event;
-      success += 1;
-      console.log(`  第${success}次${event}！${word}`);
+      if(showEvent) {
+        const random = ~~(Math.random() * events.length);
+        const item = events[random];
+        success += 1;
+        event = `第${success}次${item.event}！`;
+      }
+      console.log(chalk.green(`  ${event} ${word}`));
     });
   }
 }
